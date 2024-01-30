b835fbcd9d24c72310eac0bd17d52967952fb755
aryanPatel <aryanpatel1248@gmail.com>
2024-01-30 18:49:39 +0530

    practce some string day 3

---
+++ day3.py
@@ -0,0 +1,24 @@
+# 3.1 string 
+
+str1 = "hello"
+str2 = 'world'
+str3 = """india"""
+
+print (str1+str2+str3)
+
+# finde length 
+str4 = "soap"
+print (len(str4))
+
+# we can not chnge the string using indexing
+# strWord = "xyz "
+# str[4]="s"
+# print(strWord[3])
+
+#slicing
+
+word = "aryan patel"
+print(word[0:5])
+print(word[6:len(word)])
+print(word[0:])
+print(word[:5])
