6f838abf5bd06be5c59b402918d14e70454cb3f7
aryanPatel <aryanpatel1248@gmail.com>
2024-12-30 21:10:38 +0530

    revise some bignner level topic py

---
+++ easyTopic.py
@@ -0,0 +1,41 @@
+
+# 5 data type support int,str,float,boolean(True,False),none
+
+# python is case sensitive a A bothe are diff
+name = "aryan"
+age=21
+cgpa=8.43
+startWith='a'
+isPass = True
+a = None
+"""
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(startWith))
+print(type(isPass))
+print(type(a))
+"""
+# print two num sum
+
+# num1 = int(input("enter value of a: "))
+# num2 = int(input("enter value of b: "))
+# sum = num1+num2
+# print(sum)
+# type conversion if p="str" then err
+p = 1
+q = 2.0
+sum = p + q
+print(sum)
+
+# if we convert in manually it call typecast
+
+# pi = 3.14
+# print(str(pi))
+# print(type(pi))
+
+# num1 = int(input("enter number 1"))
+# num2 = int(input("enter number 2"))
+# sum = num1 + num2
+# print(sum)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,5 @@
-personAge = input("Enter your age")
-# print("your age is ",personAge,type(personAge)
+
+# num1 = int(input("enter value of a: "))
+# num2 = int(input("enter value of b: "))
+# sum = num1+num2
+# print(sum)
