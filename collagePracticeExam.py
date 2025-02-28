0993ab129f7acbf1eaf48a0402e93cadbf1b839d
aryanPatel <aryanpatel1248@gmail.com>
2025-02-28 22:44:03 +0530

    some solve guess th output of question

---
+++ collagePracticeExam.py
@@ -0,0 +1,46 @@
+# T1=(10)
+# print(T1*2)
+# T2=(10,0)
+# print(T2*2)
+#20 (10,0,10,0)
+
+# L=[10,20,30,20,30,20,30,10,30]
+# print(L.count(30))
+# print(L.index(20))
+# print(L.index(20,4))
+#4 1 5 
+
+# def s(x):
+#     return (x+x)
+# for n in [1,2,3]:
+#     print(s(n))
+# 2 4 6
+
+# p = {'AA','BB','CC','DD'}
+# q = {'BB','CC','EE'}
+# print(p.difference(q))
+# print(p.symmetric_difference(q))
+
+# AA DD , AA DD EE
+
+# a = 'Python Subject'
+# print(a[8:2:-1])
+# print(a[2:9:1])
+#uS noh , thon Su
+
+# a = {12}
+# print(type(a))
+# b={}
+# print(type(b))
+
+#  replaxe x to show output pqr#pqr 
+#  print('pqr',x)
+#  print('pqr')
+
+# L = list('123456')
+# L0=L5=0
+# L3=L[-2]
+# print(L)
+
+l=[i**2 for i in range(1,5)]
+print(l)
