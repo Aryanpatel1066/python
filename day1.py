ceb2fe7f63d3bd841463a7a5293c71716b15d390
Aryanpatel1066 <aryanpatel1248@gmail.com>
2025-08-26 14:39:01 +0530

    fist commit of the day

---
+++ day1.py
@@ -0,0 +1,35 @@
+# for x in "banana":
+#  print(x)
+# my_list = [1,2,"hello"]
+# print(my_list[2])
+# r = list(range(5))
+# print(r)
+# fruits = ["apple","banana","xyz"]
+# firstFruit =fruits[0]="graps"
+# print(fruits)
+# print(firstFruit)
+
+# x = "hello"
+# y = 5
+# print(7+9)
+
+# squres = []
+
+# for x in range(5):
+#     squres.append(x**2)
+# print(squres)
+
+# unpac the tupple 
+
+fruits = ("apple","banana","cheryry")
+(a,b,c)=fruits
+print(a,b,c)
+
+num = {1,2,3,4,1}
+print(num)
+
+fruits2 = {"apple", "banana", "cherry"}
+
+print(fruits2)  
+# Output could be: {'banana', 'cherry', 'apple'}
+# Or: {'cherry', 'apple', 'banana'}
---
+++ ifelse.py
@@ -0,0 +1,79 @@
+# Write a program that checks if a number is even or odd.
+
+# number = int(input("enter a number"))
+# if number % 2 == 0:
+#   print("number is even")
+# else: 
+#   print("number is odd")
+
+
+# print(type(number))
+
+# Positive, Negative, or Zero
+# Take a number from the user and check if it is positive, negative, or zero
+
+# num = int(input("enter a number"))
+
+# if (num >0):
+#     print("the number is positive")
+# elif num < 0:
+#     print("the number is negetive")
+# else:
+#     print("this is 0")
+
+# Largest of Two Numbers
+# Ask for two numbers and print the larger one.
+
+# number1 = int(input("enter first number"))
+# number2 = int(input("enter second number"))
+# print(number1) if (number1>number2) else print(number2)
+
+#now check big number out of 3 number
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+
+# if num1 > num2:
+#     if num1 > num3:
+#         print(f"{num1} is the largest")
+#     else:
+#         print(f"{num3} is the largest")
+# else:
+#     if num2 > num3:
+#         print(f"{num2} is the largest")
+#     else:
+#         print(f"{num3} is the largest")
+
+# Leap Year Checker
+# Take a year as input and check if it is a leap year (divisible by 4, but not 100 unless also divisible by 400).
+
+# year = int(input("Enter year: "))
+
+# if year % 400 == 0:
+#     print("This is a leap year")
+# elif year % 100 == 0:
+#     print("This is not a leap year")
+# elif year % 4 == 0:
+#     print("This is a leap year")
+# else:
+#     print("This is not a leap year")
+
+# switch case
+
+# days = 5
+
+# match days:
+#     case 1:
+#         print("the 1")
+#     case 2:
+#         print("the 2")
+#     case 3:
+#         print("the 3")
+#     case _:
+#         print("sorry did not found the day")
+
+
+
+
+ 
